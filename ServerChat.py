7dab60dff04d65a6afda476771fd16104757bbcd
SeanBabanov <165194572+SeanBabanov@users.noreply.github.com>
2024-06-24 16:59:50 +0300

    ServerChat.py

--- ServerChat.py
+++ ServerChat.py
@@ -1,38 +1,26 @@
 import socket
+import socket
 import threading
-import sqlite3
 
-def save_message(client_address, message):
-    conn = sqlite3.connect('messages.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-    INSERT INTO messages (client_address, message) VALUES (?, ?)
-    ''', (client_address, message))
-    conn.commit()
-    conn.close()
+def handle_client(conn, addr):
+    print(f"Connected by {addr}")
+    with conn:
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+    print(f"Connection with {addr} closed")
 
-def handle_client(client_socket, client_address):
-    while True:
-        message = client_socket.recv(1024).decode()
-        if not message:
-            break
-        print("Client:", message)
-        client_socket.sendall(message.encode())
-        save_message(client_address, message)
-    client_socket.close()
-
-def main():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    PORT = 12345
-    server_socket.bind(('localhost', PORT))
-    server_socket.listen(5)
-    print("Server is listening for connections...")
-
-    while True:
-        client_socket, client_address = server_socket.accept()
-        print("Connection established with", client_address)
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address[0]))
-        client_thread.start()
+def start_server(host='localhost', port=12345):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        s.listen()
+        print(f"Server has started, listening on {host}:{port}")
+        while True:
+            conn, addr = s.accept()
+            client_thread = threading.Thread(target=handle_client, args=(conn, addr))
+            client_thread.start()
 
 if __name__ == "__main__":
-    main()
+    start_server()
